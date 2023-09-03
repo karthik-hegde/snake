c5de32cde845c464f1416301256b8006373fc6ea
karthik-hegde <karthikhegde77@gmail.com>
2023-09-03 19:41:32 +0530

    added key events

--- src/app/Objects/snake.ts
+++ src/app/Objects/snake.ts
@@ -1,23 +1,43 @@
 import * as p5 from "p5";
 
-export class Snake{
+export class Snake {
     private x: number = 0;
     private y = 0;
     private xspeed = 0;
     private yspeed = 0;
-    
+
     constructor() {
         this.xspeed = 1;
     }
-   
-    public update(): void{
+
+    public update(): void {
         this.x = this.x + this.xspeed;
         this.y = this.y + this.yspeed;
     }
 
-    public show(p: p5): void{
+    public show(p: p5): void {
         p.fill(255);
-        p.rect(this.x,this.y,10,10);
+        p.rect(this.x, this.y, 20, 20);
     }
 
+    public changeDirection(dir: string) {
+        if (this.yspeed) {
+            if (dir === "LEFT") {
+                this.updateDirection(-1, 0);
+            } else if (dir === "RIGHT") {
+                this.updateDirection(1, 0);
+            }
+        } else if (this.xspeed) {
+            if (dir === "UP") {
+                this.updateDirection(0, -1);
+            } else if (dir === "DOWN") {
+                this.updateDirection(0, 1);
+            }
+        }
+    }
+
+    private updateDirection(x: number, y: number): void {
+        this.xspeed = x ?? this.xspeed;
+        this.yspeed = y ?? this.yspeed;
+    }
 }
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,16 +7,17 @@ import { Snake } from '../Objects/snake';
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.scss']
 })
-export class BoardComponent implements OnInit{
+export class BoardComponent implements OnInit {
   private canvas!: p5;
-  constructor(){}
+  constructor() { }
 
   ngOnInit(): void {
     this.setupCanvas();
   }
 
-  private setupCanvas(): void{
-     this.canvas = new p5(this.sketch);
+  private setupCanvas(): void {
+    this.canvas = new p5(this.sketch);
+
   }
 
   private sketch(p: p5) {
@@ -24,13 +25,25 @@ export class BoardComponent implements OnInit{
     p.setup = () => {
       p.createCanvas(600, 600);
     };
-  
+
     p.draw = () => {
       p.background(51);
       snake.update();
       snake.show(p);
     };
-  }
 
+    p.keyPressed = (key: KeyboardEvent) => {
+      console.log(key);
+      if(key.key === "ArrowLeft") {
+        snake.changeDirection("LEFT");
+      } else if(key.key === "ArrowRight"){
+        snake.changeDirection("RIGHT");
+      } else if(key.key === "ArrowUp"){
+        snake.changeDirection("UP");
+      } else if(key.key === "ArrowDown"){
+        snake.changeDirection("DOWN");
+      }
+    }
+  }
 
 }
